a61921d2c122e847e6ab1a442ed8c60f538be19f
jasper.lai <jasper.lai@gmail.com>
2021-02-15 17:30:05 +0800

    CookiesDb: 申實體資料庫匯入結構

---
+++ CookiesDb/dbo/Tables/Categories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [CategoryId]   INT           NOT NULL,
+    [CategoryName] NVARCHAR (30) NOT NULL,
+    CONSTRAINT [PK_Catgories] PRIMARY KEY CLUSTERED ([CategoryId] ASC)
+);
+
---
+++ CookiesDb/dbo/Tables/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT           NOT NULL,
+    [ProductName] NVARCHAR (30) NOT NULL,
+    [CategoryId]  INT           NOT NULL,
+    [Price]       INT           NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC),
+    CONSTRAINT [FK_Categories] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([CategoryId])
+);
+
