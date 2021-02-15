452f4b41f06878088a6ddb5eca16638af18c84e7
jasper.lai <jasper.lai@gmail.com>
2021-02-15 17:44:00 +0800

    CookiesDb: 1. 異動 Products.Price 欄位資料型態, 2. 加入 View

--- CookiesDb/dbo/Tables/Products.sql
+++ CookiesDb/dbo/Tables/Products.sql
@@ -2,7 +2,7 @@
     [ProductId]   INT           NOT NULL,
     [ProductName] NVARCHAR (30) NOT NULL,
     [CategoryId]  INT           NOT NULL,
-    [Price]       INT           NOT NULL,
+    [Price]       DECIMAL(6, 2)           NOT NULL,
     CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC),
     CONSTRAINT [FK_Categories] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([CategoryId])
 );
---
+++ CookiesDb/dbo/Views/ViewProducts.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[ViewProducts]
+AS 
+    SELECT  P.ProductId,
+            P.ProductName,
+            P.Price,
+            P.CategoryId,
+            C.CategoryName
+    FROM    [Products] AS P LEFT JOIN [Categories] AS C 
+                       ON ( P.CategoryId = C.CategoryId )
